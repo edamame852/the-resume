ccac6de28db89c9ea62608e32c24c24e24406555
Miller <71180530+edamame852@users.noreply.github.com>
2024-08-13 23:52:44 +0800

    Update data.jsx

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -49,7 +49,7 @@ export const SectionId = {
   Resume: 'resume',
   Skills: 'skills',
   Stats: 'stats',
-  Testimonials: 'testimonials',
+  Testimonials: 'testimonials'
 }
 
 // export type SectionId = (typeof SectionId)[keyof typeof SectionId];
